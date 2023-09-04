55511e94f2723f2f4c190f1ef2dc42e1fc3116e7
ashsajal1 <ashsajal1@gmail.com>
2023-09-04 16:57:39 +0800

    first commit

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+  styleUrls: ['./card.component.css'],
 })
 export class CardComponent {
+  @Output() onClickEvent = new EventEmitter();
 
+  @Input({ required: true }) data!: string[];
+
+  onClick(): void {
+    this.onClickEvent.emit(this.data);
+  }
 }
